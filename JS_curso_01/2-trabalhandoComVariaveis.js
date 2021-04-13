918b569434bcdef1e479e9c6307ce4cf81e461e2
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2021-04-13 10:59:37 -0300

    capitulo 02 parcial

---
+++ JS_curso_01/2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+// Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é case sensitive
+
+const idade = 29;
+const Idade = 30;
+const nome = "Fábio";
+let ano = 2020;
+
+console.log(idade);
+console.log(Idade);
