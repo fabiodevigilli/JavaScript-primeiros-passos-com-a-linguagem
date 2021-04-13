435f4e6f76c1e794f315ec1b0e3c21ee4417e2ff
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2021-04-13 10:23:34 -0300

    primeiro capitulo finalizado

---
+++ JS_curso_01/1-meuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 37;
+
+console.log("idade:",idade);
+console.log("idade+2:",idade+2);
+
+const idadeSomada = idade+2;
+console.log("idade somada:",idadeSomada);
